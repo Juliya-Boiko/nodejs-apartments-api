9fdcdf265fdfa4546cfac6185cbfbf94718a4823
Juliya Boiko <boikojuliya2707@gmail.com>
2023-02-02 22:57:44 +0200

    add server

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-// const mongoose = require('mongoose');
-
-// const DB_HOST = 'mongodb+srv://Juliya:zerSfUoinWhfC8Z4@cluster0.vyrnwnv.mongodb.net/apartmentsDB?retryWrites=true&w=majority';
-
-
-// mongoose.connect(DB_HOST)
-//   .then(() => {
-//     console.log("Database connection successful");
-//     app.listen(PORT);
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
-
+const mongoose = require('mongoose');
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-});
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// });
+
+// console.log('DB_HOST --->', DB_HOST);
